0745c2953b1000ebdbdf554401a0ab977922c993
Crony Akatsuki <crolinuxgamer@gmail.com>
2022-07-16 21:04:40 +0200

    My personal config.

--- config.h
+++ config.h
@@ -1,16 +1,14 @@
 #define CMDLENGTH 45
-#define DELIMITER "  "
-#define CLICKABLE_BLOCKS
+#define DELIMITER " "
+/* #define CLICKABLE_BLOCKS */
 
 const Block blocks[] = {
-	BLOCK("sb-mail",    1800, 17),
-	BLOCK("sb-music",   0,    18),
-	BLOCK("sb-disk",    1800, 19),
-	BLOCK("sb-memory",  10,   20),
-	BLOCK("sb-loadavg", 5,    21),
-	BLOCK("sb-mic",     0,    26),
-	BLOCK("sb-record",  0,    27),
-	BLOCK("sb-volume",  0,    22),
-	BLOCK("sb-battery", 5,    23),
-	BLOCK("sb-date",    1,    24)
+    BLOCK("sb-optimus", 0, 0),
+    BLOCK("sb-cpu", 4, 0),
+    BLOCK("sb-cpu-temp", 4, 0),
+    BLOCK("sb-ram", 4, 0),
+    BLOCK("sb-disk-space", 1600, 0),
+    BLOCK("sb-wifi", 5, 0),
+	BLOCK("sb-battery", 10,    0),
+	BLOCK("sb-datetime",    30,    0)
 };
