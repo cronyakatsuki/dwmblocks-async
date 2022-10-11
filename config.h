a59e70cfbf3bcd44724971f1f7cba0b1c19dec51
cronyakatsuki <cronyakatsuki@tutanota.com>
2022-10-11 16:23:10 +0200

    Make scripts update less frequiently.

--- config.h
+++ config.h
@@ -5,11 +5,11 @@
 const Block blocks[] = {
     BLOCK("sb-bluetooth", 5, 0),
     BLOCK("sb-optimus", 0, 0),
-    BLOCK("sb-cpu", 4, 0),
-    BLOCK("sb-cpu-temp", 4, 0),
-    BLOCK("sb-ram", 6, 0),
+    BLOCK("sb-cpu", 8, 0),
+    BLOCK("sb-cpu-temp", 15, 0),
+    BLOCK("sb-ram", 9, 0),
     BLOCK("sb-disk-space", 1600, 0),
-    BLOCK("sb-wifi", 3, 0),
-	BLOCK("sb-battery", 8,    0),
+    BLOCK("sb-wifi", 5, 0),
+	BLOCK("sb-battery", 10,    0),
 	BLOCK("sb-datetime",    30,    0)
 };
