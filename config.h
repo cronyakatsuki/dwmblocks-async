0912e271b861b029d5d4b17c22a4b061746254eb
cronyakatsuki <cronyakatsuki@tutanota.com>
2022-10-14 12:00:03 +0200

    Disabled unnedded scripts.

--- config.h
+++ config.h
@@ -3,11 +3,11 @@
 /* #define CLICKABLE_BLOCKS */
 
 const Block blocks[] = {
-    BLOCK("sb-bluetooth", 5, 0),
-    BLOCK("sb-optimus", 0, 0),
-    BLOCK("sb-cpu", 8, 0),
+    /* BLOCK("sb-bluetooth", 5, 0), */
+    /* BLOCK("sb-optimus", 0, 0), */
+    /* BLOCK("sb-cpu", 8, 0), */
     BLOCK("sb-cpu-temp", 15, 0),
-    BLOCK("sb-ram", 9, 0),
+    /* BLOCK("sb-ram", 9, 0), */
     BLOCK("sb-disk-space", 1600, 0),
     BLOCK("sb-wifi", 5, 0),
 	BLOCK("sb-battery", 10,    0),
